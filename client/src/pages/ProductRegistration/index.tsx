c0ce27bef647d2c5d385ef50e7bd2ba6041752c4
loloara <addy94@naver.com>
2019-11-14 16:12:15 +0900

    apit =est:

--- client/src/pages/ProductRegistration/index.tsx
+++ client/src/pages/ProductRegistration/index.tsx
@@ -124,14 +124,15 @@ const ProductRegistration = inject(STORES.PRODUCTS_STORE)(observer((props: Injec
             <label>차량 판매자 흡연 여부</label>
             <div className="form-check form-check-inline form-check-smoking">
               <input className="form-check-input" type="radio" name="smokingOptions" id="inlineSmoker"
-                value="true" onChange={v =>  {setSmoking(Boolean(v.target.value))
+                value="true" onChange={v =>  {
+                setSmoking(v.target.value);
                 console.log('radiotrue', v.target.value);
                 }} required />
                 <label className="form-check-label smoker" htmlFor="inlineSmoker">예, 흡연자 입니다.</label>
             </div>
             <div className="form-check form-check-inline form-check-nonsmoking">
               <input className="form-check-input" type="radio" name="smokingOptions" id="inlineNonSmoker"
-                value="false" onChange={v =>  {setSmoking(Boolean(v.target.value))
+                value="false" onChange={v =>  {setSmoking(v.target.value)
                 console.log('radiofalse', v.target.value);
               }
                 } />
