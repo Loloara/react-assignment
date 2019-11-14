6c4d0e6ce356e0c234193b16ba599ef5fed6ee11
loloara <addy94@naver.com>
2019-11-14 10:46:08 +0900

    make getCategoryName function

--- client/src/pages/utils.tsx
+++ client/src/pages/utils.tsx
@@ -2,6 +2,14 @@ export function getCategoryName(categoryNumber: number): string {
   switch (categoryNumber) {
     case 0:
       return '차량';
+    case 1:
+      return '인기매물'
+    case 2:
+      return '가구/인테리어'
+    case 3:
+      return '유야동/유아도서'
+    case 4:
+      return '생활/가공식품'
     default:
       return '';
   }
