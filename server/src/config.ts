ee422fe174a854043a3415127877edc1b72561ef
loloara <addy94@naver.com>
2019-11-15 19:06:49 +0900

    temporary change for the local test

--- server/src/config.ts
+++ server/src/config.ts
@@ -3,6 +3,7 @@ export default {
     key: process.env.AUTH_KEY || 'prgms-daangn-key',
   },
   db: {
-    url: process.env.MYSQL_URL || 'mysql://root:password@mysql-server:3306/my_database',
+    //url: process.env.MYSQL_URL || 'mysql://root:password@mysql-server:3306/my_database',
+    url: process.env.MYSQL_URL || 'mysql://root:password@localhost:3306/my_database',
   },
 };
