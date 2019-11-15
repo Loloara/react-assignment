a4efd8ba2606b6900207329590a31413768980ee
loloara <addy94@naver.com>
2019-11-15 23:08:10 +0900

    add porps to detect page name

--- client/src/components/FixedTopBar/index.tsx
+++ client/src/components/FixedTopBar/index.tsx
@@ -3,19 +3,23 @@ import { Link } from 'react-router-dom';
 import { PAGE_PATHS, STORES } from '~constants';
 import { inject, observer } from 'mobx-react';
 import ProductsStore from '~stores/product/ProductStore';
-import FilterModal from '~components/FilterModal'
+import Modal from '~components/Modal';
+import FilterModal from '~components/Modal/FilterModal';
+import {getCategoryName} from '~pages/utils';
 // @ts-ignore
 import LogoImage from '~assets/logo-basic.svg';
+
 
 type FixedTopBarProps = {
   [STORES.PRODUCTS_STORE]: ProductsStore;
 }
-
+ 
 function FixedTopBar(props: FixedTopBarProps) {
   const [openModal, setOpenModal] = useState(false);
   const showFilterButton = props[STORES.PRODUCTS_STORE].categoryOfPage === 0 ? true : false;
   const filtered = props[STORES.PRODUCTS_STORE].filteredCar;
-  
+  const title = getCategoryName(props[STORES.PRODUCTS_STORE].categoryOfPage) + " 조건 설정";
+
   return (
     <nav className="navbar nav-global fixed-top navbar-expand-sm">
       <div className="container">
@@ -28,9 +32,12 @@ function FixedTopBar(props: FixedTopBarProps) {
             <button className={filtered ? "btn-filter active" : "btn-filter"} onClick={() => {setOpenModal(true)}}>
               <i className="material-icons ic-filter">filter_list</i>
             </button>
-            <FilterModal isOpen={openModal} />
+            <Modal title={title} open={openModal} onClose={setOpenModal}>
+              <FilterModal />
+            </Modal>
           </li> }
           <li className="nav-item">
+            {console.log('before the page regi')}
             <Link to={PAGE_PATHS.PRODUCT_REGISTRATION} >
               <i className="material-icons ic-create">create</i>
             </Link>
