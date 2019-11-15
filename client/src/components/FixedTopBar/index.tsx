7bdf0fd35ab606416739b5ad20ca0a80160e9c91
loloara <addy94@naver.com>
2019-11-15 20:22:20 +0900

    attach FilterModal tag

--- client/src/components/FixedTopBar/index.tsx
+++ client/src/components/FixedTopBar/index.tsx
@@ -1,10 +1,21 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { PAGE_PATHS } from '~constants';
+import { PAGE_PATHS, STORES } from '~constants';
+import { inject, observer } from 'mobx-react';
+import ProductsStore from '~stores/product/ProductStore';
+import FilterModal from '~components/FilterModal'
 // @ts-ignore
 import LogoImage from '~assets/logo-basic.svg';
 
-function FixedTopBar() {
+type FixedTopBarProps = {
+  [STORES.PRODUCTS_STORE]: ProductsStore;
+}
+
+function FixedTopBar(props: FixedTopBarProps) {
+  const [openModal, setOpenModal] = useState(false);
+  const showFilterButton = props[STORES.PRODUCTS_STORE].categoryOfPage === 0 ? true : false;
+  const filtered = props[STORES.PRODUCTS_STORE].filteredCar;
+  
   return (
     <nav className="navbar nav-global fixed-top navbar-expand-sm">
       <div className="container">
@@ -13,11 +24,12 @@ function FixedTopBar() {
                src={LogoImage} />
         </Link>
         <ul className="navbar-nav ml-auto">
-          <li className="nav-item">
-            <button className="btn-filter" data-toggle="modal" data-target="#section-filter">
+          { showFilterButton && <li className="nav-item">
+            <button className={filtered ? "btn-filter active" : "btn-filter"} onClick={() => {setOpenModal(true)}}>
               <i className="material-icons ic-filter">filter_list</i>
             </button>
-          </li>
+            <FilterModal isOpen={openModal} />
+          </li> }
           <li className="nav-item">
             <Link to={PAGE_PATHS.PRODUCT_REGISTRATION} >
               <i className="material-icons ic-create">create</i>
@@ -29,4 +41,4 @@ function FixedTopBar() {
   );
 }
 
-export default FixedTopBar;
+export default inject(STORES.PRODUCTS_STORE)(observer(FixedTopBar));
